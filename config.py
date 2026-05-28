d221be78d943baaeff9bf527b5584bfedc8b2221
txglobalpro <txglobalpro@example.com>
2026-05-28 20:21:31 +0300

    social bar: keep Telegram + Website only, cleaner look

--- config.py
+++ config.py
@@ -22,9 +22,7 @@ class Config:
 
     SOCIAL_LINKS = [
         {'key': 'telegram', 'url': os.environ.get('TELEGRAM_CHANNEL_URL', 'https://t.me/eloniinvest'), 'icon': 'bi-telegram', 'label': 'Telegram', 'color': '#08c'},
-        {'key': 'facebook', 'url': os.environ.get('FACEBOOK_URL', '#'), 'icon': 'bi-facebook', 'label': 'Facebook', 'color': '#1877f2'},
-        {'key': 'twitter', 'url': os.environ.get('TWITTER_URL', '#'), 'icon': 'bi-twitter-x', 'label': 'X', 'color': '#fff'},
-        {'key': 'website', 'url': os.environ.get('WEBSITE_URL', 'https://eloninvest.publicvm.com'), 'icon': 'bi-globe2', 'label': 'Website', 'color': '#f0b90b'},
+        {'key': 'website', 'url': os.environ.get('WEBSITE_URL', '#'), 'icon': 'bi-globe2', 'label': 'Website', 'color': '#f0b90b'},
     ]
 
     WTF_CSRF_ENABLED = True
